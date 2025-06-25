f21fb7a40477800b373e22174acf4baa589cc266
sarthakkhedkar8 <sarthakkhedkar4@gmail.com>
2025-06-25 18:20:00 +0530

    Create problem6invertedleftpyramid.java

---
+++ problem6invertedleftpyramid.java
@@ -0,0 +1,11 @@
+public class problem6invertedleftpyramid {
+    public static void main (String[] args){
+        for (int j = 0 ; j <=5 ; j++){
+            System.out.println(" ");
+            for (int i = 0 ; i < 5 - j ; i++){
+                System.out.print("*");
+            }
+        }
+    }
+    
+}
