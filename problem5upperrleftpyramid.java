1445e4ef75609050a70a2eb2b7e8a52fa214d6d1
sarthakkhedkar8 <sarthakkhedkar4@gmail.com>
2025-06-25 18:19:33 +0530

    Create problem5upperrleftpyramid.java

---
+++ problem5upperrleftpyramid.java
@@ -0,0 +1,12 @@
+public class problem5upperrleftpyramid {
+    public static void main(String[] args){
+        for (int i = 0 ; i<5 ; i++){
+            System.out.println(" ");
+            for (int j = 0 ; j<i+1 ; j++){
+                System.out.print("*");
+            }
+        }
+
+    }
+    
+}
