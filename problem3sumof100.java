f6e8c7d71693033e558bc608339d64bab60c9b51
sarthakkhedkar8 <sarthakkhedkar4@gmail.com>
2025-06-25 18:18:35 +0530

    Create problem3sumof100.java

---
+++ problem3sumof100.java
@@ -0,0 +1,10 @@
+public class problem3sumof100 {
+        public static void main (String[] args){
+        int sum = 0 ;
+
+        for (int i = 1; i <= 100; i++) {
+            sum=sum + i;      
+        }
+        System.out.println("The sum of the 1 to 100 is   "+ sum);
+    }
+}
