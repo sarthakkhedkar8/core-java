7aaab3504df5bc89a7117a237744610d57383660
sarthakkhedkar8 <sarthakkhedkar4@gmail.com>
2025-06-25 18:18:09 +0530

    Create problem2reversetill10to1.java

---
+++ problem2reversetill10to1.java
@@ -0,0 +1,10 @@
+public class problem2reversetill10to1 {
+    public static void main (String[] args){
+        for (int i = 10 ; i>0 ; i--){
+            System.out.println(i);
+            // Decrement i to avoid infinite loop
+
+        }
+    }
+    
+}
