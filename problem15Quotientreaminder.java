929a51d0f3fb10bbf66c70e9cc751293e6d4d302
sarthakkhedkar8 <sarthakkhedkar4@gmail.com>
2025-06-25 18:14:48 +0530

    Create problem15Quotientreaminder.java

---
+++ problem15Quotientreaminder.java
@@ -0,0 +1,14 @@
+public class problem15Quotientreaminder {
+    public static void main(String[] args) {
+        int dividend = 10; // Example dividend
+        int divisor = 3;   // Example divisor
+
+        // Calculate quotient and remainder
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        // Output the results
+        System.out.println("The quotient of " + dividend + " divided by " + divisor + " is: " + quotient);
+        System.out.println("The remainder of " + dividend + " divided by " + divisor + " is: " + remainder);
+    }
+}
