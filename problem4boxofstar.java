92d4b0c8865d4d60fe3e273391facaf4daf84c3b
sarthakkhedkar8 <sarthakkhedkar4@gmail.com>
2025-06-25 18:19:03 +0530

    Create problem4boxofstar.java

---
+++ problem4boxofstar.java
@@ -0,0 +1,12 @@
+public class problem4boxofstar {
+    public static void main (String[] args){
+        int i;
+        // Print 5 asterisks using a for loop   
+        for(i=0 ; i<5 ; i++){
+            System.out.println(' ');
+            for(int j=0 ; j<5 ; j++){
+                System.out.print('*');
+            }
+        }
+    }
+}
